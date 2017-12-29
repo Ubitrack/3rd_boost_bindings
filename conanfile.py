6fcecc4049c20e669e571efac7e57b3d670fa1e1
NARVIS-UECK\ueck <ulrich.eck@tum.de>
2017-12-29 13:17:37 +0100

    moved headers to include directory, updated cmake files and conan build script

--- conanfile.py
+++ conanfile.py
@@ -9,15 +9,11 @@ class UbitrackboostbindingsConan(ConanFile):
     url = "https://github.com/Ubitrack/3rd_boost_bindings"
     description = "Boost Bindings is a bindings library (not just) for Boost.Ublas. It offers an easy way of calling BLAS, LAPACK, UMFPACK, MUMPS and many other mature legacy numerical codes from within C++."
     no_copy_source = True
+    exports_sources = "include/*"
     # No settings/options are necessary, this is header only
 
-    def source(self):
-        '''retrieval of the source code here. Remember you can also put the code in the folder and
-        use exports instead of retrieving it with this source() method
-        '''
-        #self.run("git clone ...") or
-        #tools.download("url", "file.zip")
-        #tools.unzip("file.zip" )
-
     def package(self):
-        self.copy("*.h", dst="include/boost", src="boost")
+        self.copy("*.h")
+
+    def package_id(self):
+        self.info.header_only()
