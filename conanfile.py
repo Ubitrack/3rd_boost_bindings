16dc68e6d3c60d7d6d406382583c9b294ae80594
Ulrich Eck <ulrich.eck@tum.de>
2018-07-09 17:05:16 +0200

    add missing changes

--- conanfile.py
+++ conanfile.py
@@ -1,20 +1,25 @@
-from conans import ConanFile, tools
+from conans import ConanFile, CMake, tools
 import os
 
 
 class UbitrackboostbindingsConan(ConanFile):
     name = "ubitrack_boost_bindings"
     version = "1.0"
+    settings = "os", "compiler", "arch", "build_type"
     license = "https://mathema.tician.de/software/boost-numeric-bindings/"
     url = "https://github.com/Ubitrack/3rd_boost_bindings"
     description = "Boost Bindings is a bindings library (not just) for Boost.Ublas. It offers an easy way of calling BLAS, LAPACK, UMFPACK, MUMPS and many other mature legacy numerical codes from within C++."
     no_copy_source = True
-    exports_sources = "include/*"
-    # No settings/options are necessary, this is header only
+    exports_sources = "include/*", "CMakeLists.txt", "ubitrack_boost_bindingsConfig.cmake"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        cmake.install()
 
     def package(self):
-        self.copy("*.h")
-        self.copy("*.hpp")
+        pass
 
     def package_id(self):
         self.info.header_only()
